fa0237aea7b30526f08a34423a1773ff32461944
TomasDanielDasilva <tomas.dasilva@suricata.la>
2023-02-09 17:04:03 -0300

    add memotest project

--- memotest/src/App.jsx
+++ memotest/src/App.jsx
@@ -1,34 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import confetti from "canvas-confetti";
+
+const IMAGES = [
+  "https://icongr.am/devicon/android-original.svg?size=100&color=000000",
+  "https://icongr.am/devicon/apple-original.svg?size=100&color=ffffff",
+  "https://icongr.am/devicon/css3-original.svg?size=100&color=000000",
+  "https://icongr.am/devicon/firefox-original.svg?size=100&color=000000",
+  "https://icongr.am/devicon/git-original.svg?size=100&color=000000",
+  "https://icongr.am/devicon/google-original.svg?size=100&color=000000",
+  "https://icongr.am/devicon/html5-original.svg?size=100&color=000000",
+  "https://icongr.am/devicon/javascript-original.svg?size=100&color=000000",
+  "https://icongr.am/devicon/linkedin-original.svg?size=100&color=000000",
+  "https://icongr.am/devicon/ruby-original.svg?size=100&color=000000",
+]
+  .flatMap((image) => [`a|${image}`, `b|${image}`])
+  .sort(() => Math.random() - 0.5);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [guessed, setGuessed] = useState([]);
+  const [selected, setSelected] = useState([]);
+
+  useEffect(() => {
+    if (selected.length === 2) {
+      if (selected[0].split("|")[1] === selected[1].split("|")[1]) {
+        setGuessed((guessed) => guessed.concat(selected));
+      }
+
+      setTimeout(() => setSelected([]), 1000);
+    }
+  }, [selected]);
+
+  useEffect(() => {
+    if (guessed.length === IMAGES.length) {
+      confetti();
+    }
+  }, [guessed]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>MemoTest</h1>
+      <ul className="main-container">
+        {IMAGES.map((image) => {
+          const [, url] = image.split("|");
+
+          return (
+            <li
+              key={image}
+              onClick={() =>
+                selected.length < 2 &&
+                setSelected((selected) => selected.concat(image))
+              }
+            >
+              {selected.includes(image) || guessed.includes(image) ? (
+                <img src={url} alt="icon" />
+              ) : (
+                <img
+                  key={image}
+                  src="https://icongr.am/fontawesome/rotate-right.svg?size=80&color=7b41f9"
+                  alt="icon"
+                />
+              )}
+            </li>
+          );
+        })}
+      </ul>
+
+      {guessed.length === IMAGES.length && (
+        <div className="winner-modal">
+          <div className="winner-text">
+            <h2>You win!!</h2>
+            <button
+              onClick={() => {
+                location.reload();
+              }}
+            >
+              Resetear Juego
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
