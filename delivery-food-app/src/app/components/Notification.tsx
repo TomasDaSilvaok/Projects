dbde7e43dec734cf3cae894073d4b622e83f021b
TomasDaSilvaok <tomasdaniel.dasilva@gmail.com>
2023-07-19 17:35:32 -0300

    adding assets and stiling notification component

--- delivery-food-app/src/app/components/Notification.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Notification = () => {
-  return (
-    <div className='flex items-center justify-center h-10 bg-red-400 text-white text-sm font-semibold'>Notification</div>
-  )
-}
-
-export default Notification
---
+++ delivery-food-app/src/components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className='px-4 py-8 sm:py-0 flex items-center justify-center h-12 bg-red-500 text-center text-gray-100 text-sm md:text-base'>Delivery gratis para todas las órdenes mayores a $1500. Ordena tu comida ahora!</div>
+  )
+}
+
+export default Notification
